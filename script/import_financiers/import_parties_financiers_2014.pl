dddbf32f5198f15eba708f8450e4172d44e4997e
Gil Magno <gilmagno@gmail.com>
2015-02-19 16:55:32 -0300

    Importações e Utils

---
+++ script/import_financiers/import_parties_financiers_2014.pl
@@ -0,0 +1,54 @@
+use strict;
+use warnings;
+use WikiPoliticos::Web::Model::DB;
+use WikiPoliticos::Util;
+use Parallel::ForkManager;
+
+my $schema = WikiPoliticos::Web::Model::DB->new->schema;
+my $csvs_basedir = $ARGV[0];
+my $fork_manager = Parallel::ForkManager->new(3);
+my %checked_cnpjfs;
+
+my $financiers_search = $schema->resultset('Financier')->search;
+while ( my $financier = $financiers_search->next) {
+    $checked_cnpjfs{ $financier->cnpjf } = 1;
+}
+
+for my $uf (@WikiPoliticos::Util::ufs) {
+    $fork_manager->start and next;
+    process_uf($uf);
+    $fork_manager->finish;
+}
+
+sub process_uf {
+    my $uf = shift;
+    my $filepath = "$csvs_basedir/receitas_partidos_2014_$uf.txt";
+    print "$filepath\n";
+    open my $fh, '<:encoding(latin1)', $filepath;
+    process_fh($fh);
+    close $fh;
+}
+
+sub process_fh {
+    my $fh = shift;
+    readline $fh; # discards first line
+    for my $line (<$fh>) { process_line($line) }
+}
+
+sub process_line {
+    my $line = shift;
+    my %donation_hash = WikiPoliticos::Util::make_donation_hash
+      ($line, $schema->resultset('PartyDonation2014'));
+
+    return if $checked_cnpjfs{ $donation_hash{doador_cnpjf} };
+    $checked_cnpjfs{ $donation_hash{doador_cnpjf} } = 1;
+
+    my $doador_nome = $donation_hash{doador_nome_receita_federal} ?
+      $donation_hash{doador_nome_receita_federal} :
+      $donation_hash{doador_nome};
+
+    WikiPoliticos::Util::add_financier
+        ( { cnpjf => $donation_hash{doador_cnpjf},
+            name  => $doador_nome },
+          $schema->resultset('Financier') );
+}
---
+++ script/import_politicians/import_politicians_2012.pl
@@ -0,0 +1,51 @@
+use strict;
+use warnings;
+use WikiPoliticos::Web::Model::DB;
+use WikiPoliticos::Util;
+use Parallel::ForkManager;
+
+my $schema = WikiPoliticos::Web::Model::DB->new->schema;
+my $csvs_basedir = $ARGV[0];
+my $fork_manager = Parallel::ForkManager->new(5);
+my %checked_cpfs;
+
+my @politicians_all = $schema->resultset('Politician')->all;
+for my $politician (@politicians_all) {
+    $checked_cpfs{$politician->cpf} = 1;
+}
+
+for my $uf (@WikiPoliticos::Util::ufs) {
+    $fork_manager->start and next;
+    process_uf($uf);
+    $fork_manager->finish;
+}
+
+sub process_uf {
+    my $uf = shift;
+    my $filepath = "$csvs_basedir/candidato/$uf/ReceitasCandidatos.txt";
+    print "$filepath\n";
+    open my $fh, '<:utf8', $filepath;
+    process_fh($fh);
+    close $fh;
+}
+
+sub process_fh {
+    my $fh = shift;
+    readline $fh; # discards first line
+    for my $line (<$fh>) { process_line($line) }
+}
+
+sub process_line {
+    my $line = shift;
+    my %donation_hash = WikiPoliticos::Util::make_donation_hash
+      ($line, $schema->resultset('CandidateDonation2012'));
+
+    return if $checked_cpfs{$donation_hash{candidato_cpf}};
+    $checked_cpfs{$donation_hash{candidato_cpf}} = 1;
+
+    WikiPoliticos::Util::add_politician
+        ( { cpf   => $donation_hash{candidato_cpf},
+            name  => $donation_hash{candidato_nome},
+            party => $donation_hash{partido_sigla}   },
+          $schema->resultset('Politician') );
+}
