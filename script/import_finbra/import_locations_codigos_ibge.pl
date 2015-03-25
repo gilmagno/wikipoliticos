3aaa0ba822168787a59fc364d07f49405106664e
Jessica Lins <jessicalins81@gmail.com>
2015-03-25 19:02:17 -0300

    Updating script 1 - import locations finbra

--- script/import_finbra/import_locations_codigos_ibge.pl
+++ script/import_finbra/import_locations_codigos_ibge.pl
@@ -7,30 +7,38 @@ use Data::Dumper;
 my $schema = WikiPoliticos::Web::Model::DB->new->schema;
 my $csvs_basedir = $ARGV[0];
 
-my %hash_id_tokens;
 my $finbra_locations_rs = $schema->resultset('Location')->search_rs;
+my %municipios_csv = municipios_from_csv();
 
 while (my $location = $finbra_locations_rs->next) {
-		my $pais_uf = substr $location->id, 0, 6;
-		my $municipio_token = $location->id;
-		$municipio_token =~ s/$pais_uf//;
-    $hash_id_tokens{ $municipio_token } .= $location->id;
-}
-
-open my $fh, '<:encoding(utf8)', $csvs_basedir;
-
-while (my $line = <$fh>) {
-	next if $line =~ /^CD/;
-	my @array = split /,/, $line;
-	$array[2] =~ s/^"//; 
-	$array[2] =~ s/"$//; 
-	$array[2] = WikiPoliticos::Util::make_token($array[2]); # Tokenizando o nome do municipio para comparação
-	$array[2] =~ s/-/_/g;
-	print "$array[2] \n";
-
-	if ($hash_id_tokens{ $array[2] }) { # A localidade existe no banco
-		## todo
+	my $pais_uf = substr $location->id, 0, 6;
+	my $municipio_token = $location->id;
+	$municipio_token =~ s/$pais_uf//; # Retirando o prefixo pais-uf do nome do municipio
+	
+	if (my $municipio_csv = $municipios_csv{ $municipio_token }) {
+		$location->ibge_estado_codigo($municipio_csv->{estado_codigo});
+		$location->ibge_municipio_codigo($municipio_csv->{municipio_codigo});
+		$location->update;
 	}
 }
 
-close $fh or die "$fh: $!";  
+sub municipios_from_csv {
+	open my $fh, '<:encoding(utf8)', $csvs_basedir;
+	my %municipios_csv;
+	
+	while (my $line = <$fh>) {
+		next if $line =~ /^CD/;
+		my @array = split /,/, $line;
+		$array[2] =~ s/^"//; 
+		$array[2] =~ s/"$//; 
+		my $municipio_csv_tokenized = WikiPoliticos::Util::make_token($array[2]); # Tokenizando o nome do municipio para comparação
+		# print "$municipio_csv_tokenized \n";
+		$municipios_csv{ $municipio_csv_tokenized } = 
+		{ estado_codigo => $array[0],
+			municipio_codigo => $array[1]
+		};
+	}
+
+	close $fh or die "$fh: $!"; 
+	return %municipios_csv; 
+}
